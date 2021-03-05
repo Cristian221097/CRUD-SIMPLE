fb4524a2597f4a1387baddf2b02b12798db865e0
Cristian Amancio <cristianjoelamancioherrera@gmail.com>
2021-03-06 02:02:46 +0800

    maquetando lista de empleados

---
+++ src/app/Modelos/empleado.ts
@@ -0,0 +1,13 @@
+export class Empleado {
+    constructor(
+        public id:number,
+        public nombre:string,
+        public apellido:string,
+        public edad:number,
+        public sueldo:number
+        
+    ){
+        this.id=0;
+
+    }
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Empleado} from './Modelos/empleado';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'CRUD-SIMPLE-ANGULAR';
+  
+  public empleados:Empleado[]=[
+    {id:1,nombre:'Cristian',apellido:'Amancio',edad:23,sueldo:5000},
+    {id:2,nombre:'emeli',apellido:'Amancio',edad:18,sueldo:8000},
+    {id:3,nombre:'Eunice',apellido:'herra',edad:39,sueldo:5000}
+
+  ];
+
+ 
+
+
+
+
 }
